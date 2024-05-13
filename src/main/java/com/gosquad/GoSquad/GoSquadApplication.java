44efc672edce4fafaaedcbb7ec227d1fce099556
gedeon <gedeonmutikanga@gmail.com>
2024-05-13 23:26:12 +0200

    first commit

---
+++ src/main/java/com/gosquad/GoSquad/GoSquadApplication.java
@@ -0,0 +1,13 @@
+package com.gosquad.GoSquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoSquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoSquadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gosquad/GoSquad/GoSquadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosquad.GoSquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoSquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
